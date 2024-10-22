f9adf3d8fe5f93c83ccb4bfc564ce33aa4f654a4
Hunain <syedhunainshah120@gmail.com>
2024-10-22 05:20:00 -0700

    Line number 4 Add File

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const app = express();
 
+const File = readFileSync("Index.html" , "utf-8");
+
 const Port = 80;
 const Host = "127.0.0.1";
 
 app.get("/" , (req , res)=>{
-    res.status(200).send("This Is Home Page")
+    res.status(200).send(File)
 })
 
 app.listen(Port , ()=>{
